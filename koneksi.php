b4159a49ddc6121ee41c0c8ccf4d02d393939cec
Shodik12 <67968446+Shodik12@users.noreply.github.com>
2022-07-14 02:18:29 +0700

    Update koneksi.php

--- koneksi.php
+++ koneksi.php
@@ -1,7 +1,7 @@
 <?php 
  //note!!
 //Ini koneksi dixamp $koneksi = mysqli_connect("localhost","root","","namadb");
-$koneksi = mysqli_connect("localhost", "root", "", "namadb");
+$koneksi = mysqli_connect("localhost", "root", "", "dbojk");
 
 // Check connection
 if (mysqli_connect_errno()){
